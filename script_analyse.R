55b3fef0249c4be3670fefe510f612140a9934b2
Sophie Donnet <sophie.donnet@inrae.fr>
2024-04-08 17:10:07 +0200

    Premier codes

---
+++ script_analyse.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(ggplot2)
+install.packages(tidytext)
+library(tm)
+library(knitr)
+library(gg)
+
+#----------------------------- 
+earthworms <- read.csv("~/WORK_ALL/RECHERCHE/TRAVAUX_RECHERCHE/Makowski/EarthwormsMetanalysis/database/metaanalyse_csv_final_wip.csv")
+countries <-  read.csv("~/WORK_ALL/RECHERCHE/TRAVAUX_RECHERCHE/Makowski/EarthwormsMetanalysis/database/Countries-Continents.csv")
+
+#--------------------- 
+names(earthworms)
+n = nrow(earthworms)
+
+earthworms <- earthworms %>% mutate(aboutEarthworms = rep(TRUE,n))
+for (i in 1:n){
+  abstract.i <- earthworms$Abstract..[i]
+  mylist.i <- strsplit(abstract.i, split = " ")[[1]]
+  if (("earthworm" %in% mylist.i) | ("earthworms" %in% mylist.i)){earthworms$aboutEarthworms[i] = TRUE}else{earthworms$aboutEarthworms[i] = FALSE} 
+}
+
+mean(earthworms$aboutEarthworms==TRUE)
+
