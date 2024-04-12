627a5092f6e21a7401adb6c9e765adf6674abba3
Alpharius411 <poubelle.pourrie7d2@gmail.com>
2024-04-12 18:02:35 +0200

    modifications apportées au script R (playground est mon fichier quand je travaille seul, l'autre est le backup mis à jour dès que le code fonctionne). J'ai géré la mise au singulier des mots, ça change pas mal l'aspect des plots

--- CodesRTextMining/playground.R
+++ CodesRTextMining/playground.R
@@ -6,7 +6,7 @@ library(ggplot2)
 library(tidytext)
 library(tm)
 library(knitr)
-library(SnowballC)
+library(SemNetCleaner)
 
 data("stop_words")
 
@@ -19,33 +19,36 @@ names(earthworms)
 n = nrow(earthworms)
 token_list <- c()
 all_words<- c()
+all_words_sg<-c()
 
 earthworms <- earthworms %>% mutate(aboutEarthworms = rep(TRUE,n))
-for (i in 1:n){
-  abstract.i <- earthworms$Abstract..[i]
-  abstracts <- c(abstracts, abstract.i)
-  for (i in seq_along(abstracts)) {
-    # Unnest tokens for the current abstract
-    unnested_tokens <- tibble(abstracts = abstracts[i]) %>%
-      unnest_tokens(word, abstracts)
-    # Append the unnested tokens to the list
-    unnested_tokens<-unnested_tokens%>%anti_join(stop_words)
-    unnested_tokens <- unnested_tokens %>%
-      mutate(word = wordStem(word))
-    token_list<-c(token_list, unnested_tokens)
-  }
-  mylist.i <- strsplit(abstract.i, split = " ")[[1]]
-  if (("earthworm" %in% mylist.i) | ("earthworms" %in% mylist.i)){earthworms$aboutEarthworms[i] = TRUE}else{earthworms$aboutEarthworms[i] = FALSE} 
+ for (i in 1:n){
+   abstract.i <- earthworms$Abstract..[i]
+   abstracts <- c(abstracts, abstract.i)
+   for (i in seq_along(abstracts)) {
+     # Unnest tokens for the current abstract
+     unnested_tokens <- tibble(abstracts = abstracts[i]) %>%
+       unnest_tokens(word, abstracts)
+     # Append the unnested tokens to the list
+     unnested_tokens<-unnested_tokens%>%anti_join(stop_words)
+     token_list<-c(token_list, unnested_tokens)
+   }
+   mylist.i <- strsplit(abstract.i, split = " ")[[1]]
+  if (("earthworm" %in% mylist.i) | ("earthworms" %in% mylist.i)){earthworms$aboutEarthworms[i] = TRUE}else{earthworms$aboutEarthworms[i] = FALSE}
 }
 
-#Here we could try to remove the "abstract" word from the token list, but given it technically appear only one per abstract, its presence
-#in the graph may give additional information about words relative frequencies in the abstracts.
 for (i in seq_along(token_list)) {
   words <- token_list[i]$word
   all_words <- c(all_words, words)
 }
 
-word_counts <- table(all_words)
+for (sg in seq_along(all_words)) {
+  word_parts <- unlist(strsplit(all_words[[sg]], " "))
+  singular_parts <- sapply(word_parts, singularize)
+  all_words_sg <- c(all_words_sg, unname(singular_parts))
+}
+
+word_counts <- table(all_words_sg)
 word_counts_df <- as.data.frame(word_counts, stringsAsFactors = FALSE)
 colnames(word_counts_df) <- c("word", "frequency")
 word_counts_df <- word_counts_df[order(-word_counts_df$frequency), ]
@@ -54,7 +57,6 @@ ggplot(word_counts_df[1:20, ], aes(x = frequency, y = reorder(word, frequency)))
   geom_bar(stat = "identity") +
   labs(x = "Frequency", y = "Words") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-all_words_sentiments<-left_join(all_words,bing,by="word")
 
 mean(earthworms$aboutEarthworms==TRUE)
 
