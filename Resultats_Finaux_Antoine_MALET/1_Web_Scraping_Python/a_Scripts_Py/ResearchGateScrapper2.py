767474c6940b44ebe261ed2daa38a1e416aff306
Alpharius411 <poubelle.pourrie7d2@gmail.com>
2024-07-16 15:46:00 +0200

    création d'un dossier contenant les résultats du stage / sauf oubli de ma part, les scripts sont commentés

---
+++ Resultats_Finaux_Antoine_MALET/1_Web_Scraping_Python/a_Scripts_Py/ResearchGateScrapper2.py
@@ -0,0 +1,72 @@
+# A script that opens a web browser instance and scrapes data from ResearchGate source code. For some reason, abstract texts were not found.
+
+from parsel import Selector
+from playwright.sync_api import sync_playwright
+import re
+import time
+
+def extract_url(text):
+    """
+    Extracts the first result between 'href="publication/' and '>'.
+
+    Args:
+        text (str): The input text containing the HTML.
+
+    Returns:
+        str: The extracted result if found, otherwise None.
+    """
+    match = re.search(r'href="publication/([^"]+)"', text)
+    if match:
+        return match.group(1)
+    return None
+
+def scrape_researchgate_publications(query: str):
+	"""
+	Scrapes ResearchGate data.
+	
+	Args: 
+	    Text(str): publication title.
+		
+	Returns:
+	    str: The extracted result (most notably publication date, doi and target url) if found, otherwise 'null'.
+	"""
+    with sync_playwright() as p:
+
+        browser = p.chromium.launch(headless=True, slow_mo=50)
+        page = browser.new_page(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36")
+        page_num = 1
+
+        while True:
+            page.goto(f"https://www.researchgate.net/search/publication?q={query}&page={page_num}")
+            selector = Selector(text=page.content())
+            
+            # a loop that uses style marks (css selector) to identify and extract desired data.
+            for publication in selector.css(".nova-legacy-c-card__body--spacing-inherit"):
+                title = publication.css(".nova-legacy-v-publication-item__title .nova-legacy-e-link--theme-bare::text").get().title()
+                title_link = f'https://www.researchgate.net{publication.css(".nova-legacy-v-publication-item__title .nova-legacy-e-link--theme-bare::attr(href)").get()}'
+                publication_type = publication.css(".nova-legacy-v-publication-item__badge::text").get()
+                publication_date = publication.css(".nova-legacy-v-publication-item__meta-data-item:nth-child(1) span::text").get()
+                publication_doi = publication.css(".nova-legacy-v-publication-item__meta-data-item:nth-child(2) span").xpath("normalize-space()").get()
+                publication_isbn = publication.css(".nova-legacy-v-publication-item__meta-data-item:nth-child(3) span").xpath("normalize-space()").get()
+                source_link = f'https://www.researchgate.net{publication.css(".nova-legacy-v-publication-item__preview-source .nova-legacy-e-link--theme-bare::attr(href)").get()}'
+
+                publications.append(publication_doi)
+                publications.append(publication_date)
+
+            # checks if next page arrow key is greyed out `attr(rel)` (inactive) and breaks out of the loop
+            if selector.css(".nova-legacy-c-button-group__item:nth-child(9) a::attr(rel)").get():
+                break
+            else:
+                page_num += 1
+                break
+        try:
+            target_url='https://www.researchgate.net/publication/'+extract_url(str(page.content()))
+            # Sleep used to escape recognition as a bot.
+            time.sleep(5)
+        except(TypeError):
+            target_url='null'
+
+        browser.close()
+        return(publications,target_url)
+    
+publications = []
---
+++ Stage/ExtractionBiblio/4_FileWebSrapper.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import numpy as np
+import itertools as itt
+from habanero import Crossref
+from unidecode import unidecode
+
+
+digit_to_month = {
+    1: 'Jan',
+    2: 'Feb',
+    3: 'Mar',
+    4: 'Apr',
+    5: 'May',
+    6: 'Jun',
+    7: 'Jul',
+    8: 'Aug',
+    9: 'Sep',
+    10: 'Oct',
+    11: 'Nov',
+    12: 'Dec'
+}
+
+# Initializing the lists we want to fill with API data.
+title_list= []
+abstract_list=[]
+journal_list = []
+language_list =[]
+pubdate_year_list = []
+pubdate_month_list = []
+doi_list = []
+doi_url_list=[]
+first_author_list=[]
+last_author_list=[]
+first_author_country=[]
+last_author_country=[]
+
+titles_file = input("Input the name of your tile file here (default:fichier_titres.txt): ") or "fichier_titres.txt"
+
+myQuery=open(titles_file)
+lines=0
+cr = Crossref()
+
+# Iterate over titles from the file
+for query_title in myQuery:
+    print(query_title)
+    lines+=1
+    title_list.append(query_title[:-1]) # The [:-1] is there to remove newline caracter from the end of the title.
+
+    # Handling servor errors.
+    try:
+        metadata = cr.works(query=query_title, limit=1)
+    except:
+        print("Server error. . .")
+        continue
+    try:
+        pubdate_year_list.append(metadata['message']['items'][0]['issued']['date-parts'][0][0])
+    except (KeyError, IndexError):
+        pubdate_year_list.append('No data')
+
+    try:
+        pubdate_month_list.append(digit_to_month[metadata['message']['items'][0]['issued']['date-parts'][0][1:][1]])
+    except (KeyError, IndexError):
+        pubdate_month_list.append('No data')
+
+    try:
+        doi_list.append(metadata['message']['items'][0]['DOI'])
+    except (KeyError, IndexError):
+        doi_list.append('No data')
+
+    try:
+        doi_url_list.append(metadata['message']['items'][0]['URL'])
+    except (KeyError, IndexError):
+        doi_url_list.append('No data')
+
+    try:
+        journal_list.append(metadata['message']['items'][0]['publisher'])
+    except (KeyError, IndexError):
+        journal_list.append('No data')
+
+    try:
+        language_list.append(metadata['message']['items'][0]['language'])
+    except (KeyError, IndexError):
+        language_list.append('No data')
+
+    try:
+        first_author_given = metadata['message']['items'][0]['author'][0]['given']
+        first_author_family = metadata['message']['items'][0]['author'][0]['family']
+        first_author_list.append(f"{first_author_given} {first_author_family}")
+    except (KeyError, IndexError):
+        first_author_list.append('No data')
+
+    try:
+        first_author_affiliation = metadata['message']['items'][0]['author'][0]['affiliation'][0]['name']
+        first_author_country.append(first_author_affiliation.split(' ')[-1])
+    except (KeyError, IndexError):
+        first_author_country.append('No data')
+
+    try:
+        last_author_given = metadata['message']['items'][0]['author'][-1]['given']
+        last_author_family = metadata['message']['items'][0]['author'][-1]['family']
+        last_author_list.append(f"{last_author_given} {last_author_family}")
+    except (KeyError, IndexError):
+        last_author_list.append('No data')
+
+    try:
+        last_author_affiliation = metadata['message']['items'][0]['author'][-1]['affiliation'][-1]['name']
+        last_author_country.append(last_author_affiliation.split(' ')[-1])
+    except (KeyError, IndexError):
+        last_author_country.append('No data')
+
+    try:
+        abstract_list.append(metadata['message']['items'][0]['abstract'])
+    except (KeyError, IndexError):
+        abstract_list.append('No data')
+
+zipped_lists = itt.zip_longest(title_list,first_author_list,last_author_list, abstract_list, journal_list, language_list, pubdate_year_list, pubdate_month_list,first_author_country,last_author_country,doi_list,doi_url_list,fillvalue='No data')
+df=pd.DataFrame(zipped_lists, columns=['Title', 'First author','Last author','Abstract', 'Journal', 'Language', 'Year', 'Month','First Author Country','Last Author Country','DOI','DOI URL'])
+
+# Title is first, url is last, allowing a clickabe url to appear in the Terminal.
+df.index = np.arange(1, len(df)+1)
+
+print(df)
+lines=str(lines)
+
+# FileName set to 'articles'by default, so that the newly named CSV file goes to the right directory.
+fileName='articles/'
+fileName+=input('Enter desired name for output file (default: dataMyWS_on_file_FILE_(nbLines)): ') or 'dataMyWS_on_file_'+titles_file+'_('+lines+'line(s))'
+fileName+='.csv'
+df.to_csv(fileName)
+
+
